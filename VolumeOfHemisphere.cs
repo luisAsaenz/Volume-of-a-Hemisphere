3ae2935a4612e08470aea7305e2ac6cac5369dd4
luisAsaenz <59874758+luisAsaenz@users.noreply.github.com>
2020-02-10 15:29:30 -0500

    Adding .cs file

---
+++ VolumeOfHemisphere.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VolumeHemisphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double pI = Math.PI;
+
+            double r;
+            r = Convert.ToDouble(Console.ReadLine());
+
+            if (r > 0)
+            {
+                r = Math.Pow(r , 3);
+                double volume = (2 * (r * pI)) / 3;
+                Console.WriteLine("The Volume is: " + volume);
+            }
+
+        }
+    }
+}
